1062d1622e6cf289f99f5a206f3a4fd9aaeb5b57
RUGyron <32088165+RUGyron@users.noreply.github.com>
2017-11-18 07:27:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 bot = telebot.TeleBot(config.token)
 
-"""
+
 @bot.message_handler(content_types=["text"])
 def repeater(message):  # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
@@ -72,7 +72,7 @@ def get_day(message):
         resp += '<b>{}</b>, {}, {}\n'.format(time, location, lesson)
 
     bot.send_message(message.chat.id, resp, parse_mode='HTML')
-
+"""
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)

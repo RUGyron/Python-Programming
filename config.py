58f5a8a6b5dd50e43aa5d05d9cceed38f454619f
RUGyron <32088165+RUGyron@users.noreply.github.com>
2017-12-02 07:40:37 +0300

    first_update

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 
 token = '470946558:AAFyyTxqiVArnTgS-GE9Z_wFIJC9FT3ePHs'
-domain = 'http://www.ifmo.ru/ru/schedule/0'
+domainSearch = 'https://www.kinopoisk.ru/index.php?kp_query='
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 YaBrowser/17.10.1.1204 Yowser/2.5 Safari/537.36'}

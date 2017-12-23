835fb78186129ee9c441e0a092a2757cd506e617
RUGyron <32088165+RUGyron@users.noreply.github.com>
2017-12-23 17:10:46 +0400

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-token = '470946558:AAFyyTxqiVArnTgS-GE9Z_wFIJC9FT3ePHs'
-domainSearch = 'https://www.kinopoisk.ru/index.php?kp_query='
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 YaBrowser/17.10.1.1204 Yowser/2.5 Safari/537.36'}
